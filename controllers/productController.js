242b3e84ff3d19d070c0c957510dbf0f6e9df6af
zafarich <zqalandarov@texnomart.uz>
2024-05-22 23:27:45 +0500

    asd

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,15 +11,6 @@ const openai = new OpenAI({
   apiKey: process.env.API_KEY,
 });
 
-exports.getAllProducts = async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
-};
-
 exports.addComment = async (req, res) => {
   console.log("req", req.body);
   try {
@@ -108,56 +99,7 @@ exports.createProduct = async (req, res) => {
   }
 };
 
-exports.getProductById = async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return res.status(404).json({message: "Product not found"});
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
-};
-
-exports.updateProduct = async (req, res) => {
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {new: true}
-    );
-    if (!updatedProduct) {
-      return res.status(404).json({message: "Product not found"});
-    }
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(400).json({message: error.message});
-  }
-};
-
-exports.deleteProduct = async (req, res) => {
-  try {
-    const product = await Product.findByIdAndDelete(req.params.id);
-    if (!product) {
-      return res.status(404).json({message: "Product not found"});
-    }
-    res.status(200).json({message: "Product deleted"});
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
-};
-
 async function calculateCaloriesWithAI(product) {
-  // const data_json = {
-  //   title: "",
-  //   image: "",
-  //   type: "",
-  //   lang: "",
-  //   comments: [],
-  //   client_id: [],
-  // };
-
   const lang = product?.lang === "uz" ? "Uzbek" : "Russian";
 
   const product_item =
--- routes/product.js
+++ routes/product.js
@@ -17,11 +17,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage});
 
-router.get("/", productController.getAllProducts);
 router.post("/", upload.single("image"), productController.createProduct);
 router.post("/add/comment", productController.addComment);
-router.get("/:id", productController.getProductById);
-router.put("/:id", productController.updateProduct);
-router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
