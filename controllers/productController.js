b2a0b24527aec9af62ea0c2ad65d2b4405e98f12
zafarich <zqalandarov@texnomart.uz>
2024-05-22 16:30:49 +0500

    asd

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,10 +3,12 @@ const path = require("path");
 const sharp = require("sharp");
 const sizeOf = require("image-size");
 const fs = require("fs");
+import {HttpsProxyAgent} from "https-proxy-agent";
 
 const {OpenAI} = require("openai");
 
 const openai = new OpenAI({
+  httpAgent: new HttpsProxyAgent("http://199.102.107.145:4145"),
   organization: process.env.ORG_ID,
   apiKey: process.env.API_KEY,
 });
